07d21c91720774a913ef869e2ef800fb4636130e
Clark <cmendoza.webdes@gmail.com>
2019-03-23 06:10:44 +0800

    html

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.side-nav');
+
+    
+    burger.addEventListener('click',()=> {
+        
+        nav.classList.toggle('nav-active');
+
+
+        burger.classList.toggle('toggle');
+   
+
+    });
+}
+
+navSlide();
