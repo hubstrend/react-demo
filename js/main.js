44f067b9e04da6aebf11e5b7e5f225c1de20ff68
Clark <cmendoza.webdes@gmail.com>
2019-03-23 08:16:20 +0800

    fix side nav

--- js/main.js
+++ js/main.js
@@ -1,18 +1,33 @@
 
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.side-nav');
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.side-nav');
 
     
-    burger.addEventListener('click',()=> {
+//     burger.addEventListener('click',()=> {
         
-        nav.classList.toggle('nav-active');
+//         nav.classList.toggle('nav-active');
 
-
-        burger.classList.toggle('toggle');
+//         burger.classList.toggle('toggle');
    
 
-    });
-}
+//     });
+// }
 
-navSlide();
+// navSlide();
+
+
+$(document).ready(function() {
+	$(document).on('click', function(e) {
+		
+		if ( $(e.target).closest('.container-nav').length > 0 ) {
+			  $('body').toggleClass('no-scroll');
+        $('.side-nav').toggleClass('open');
+        $('.side-nav-container').toggleClass('visible');
+		} else if ( $(e.target).closest('.side-nav-container').length === 0 && $('.side-nav-container').hasClass('visible') ) {
+	 		  $('body').removeClass('no-scroll');
+        $('.burger').removeClass("open");
+        $('.side-nav-container').removeClass("visible");
+		}
+	});
+});
